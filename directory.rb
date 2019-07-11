13a8b5eecfb4424c346e044d046e45173d0c1bbf
Stegosauruss <russell.wind@gmail.com>
2019-07-11 20:45:32 +0100

    Refactored directory.rb to be more declarative

--- directory.rb
+++ directory.rb
@@ -13,10 +13,23 @@ students = [
   "Norman Bates"
 ]
 
-# Prints student list
-puts "The studets of Villains Academy"
-puts "-------------"
-students.each {|student| puts student}
+# Method to print header
+def print_header
+  puts "The studets of Villains Academy"
+  puts "-------------"
+end
 
-# Printing the number of students
-puts "Overall we have #{students.count} great students"
+# Method to print student list
+def print(names)
+  names.each {|name| puts name}
+end
+
+# Method to print the number of students
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+# Calling methods in order
+print_header
+print(students)
+print_footer(students)
