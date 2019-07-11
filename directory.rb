c056b1ecf97ead6583cbb5652e25c487cc2424fa
Stegosauruss <russell.wind@gmail.com>
2019-07-11 21:02:48 +0100

    Added user input

--- directory.rb
+++ directory.rb
@@ -1,27 +1,29 @@
-# Array for students
-students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of he West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
-]
+# Method to add students to list
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 
 # Method to print header
 def print_header
-  puts "The studets of Villains Academy"
+  puts "The students of Villains Academy"
   puts "-------------"
 end
 
 # Method to print student list
 def print(names)
-  names.each {|name| puts name}
+  names.each do |name|
+    puts name
+  end
 end
 
 # Method to print the number of students
@@ -30,6 +32,7 @@ def print_footer(names)
 end
 
 # Calling methods in order
+students = input_students
 print_header
 print(students)
 print_footer(students)
